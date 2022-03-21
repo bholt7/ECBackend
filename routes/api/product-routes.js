9c319cab490245e5ca582c6e85b831b897007b30
Pain <holtbrenden@yahoo.com>
2022-03-21 14:48:13 -0600

    worked on the routes and the models

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,51 @@
 const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const { Product, product, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  // be sure to include its associated product and Tag data
+  Product.findAll({
+    include: [ {
+      model: product,
+      attributes: ['product_name']
+    },
+  {
+    model: Tag,
+    attributes: ['tag_name']
+  }]
+  })
+  .then(productData => res.json(productData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  // be sure to include its associated product and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{
+      model: product,
+      attributes: ['product_name']
+    },
+    {
+      model: Tag,
+      attributes: ['tag_name']
+    }]
+  })
+  .then(productData => res.json(productData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 // create new product
@@ -91,6 +124,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(productData => {
+    !productData ? res.status(404).json('Message: product Not Found') : res.json(productData)
+  })
+  .catch(err =>{
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
